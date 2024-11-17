3f88a391b1af9976df68c3d968f7cedb781fb015
Luiza Domingues Chaveiro Correia <163072410+luiza1205@users.noreply.github.com>
2024-11-17 18:35:45 -0300

    Create Footer.js

---
+++ src/Frontend/Segunda Entrega/Footer.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="about">
+        <h4>Sobre Nós</h4>
+        <p>Informações sobre a Inova Saúde.</p>
+      </div>
+      <div className="quick-links">
+        <h4>Links Rápidos</h4>
+        <ul>
+          <li><a href="#convênios">Convênios</a></li>
+          <li><a href="#contato">Contato</a></li>
+        </ul>
+      </div>
+      <div className="contact">
+        <h4>Contate-nos</h4>
+        <p>Email: contato@inovasaude.com.br</p>
+        <p>Telefone: (99) 99999-9999</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
