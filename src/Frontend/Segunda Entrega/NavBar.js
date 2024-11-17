9aa70ce3351705e74b15e6d61121e00f5c845df6
Luiza Domingues Chaveiro Correia <163072410+luiza1205@users.noreply.github.com>
2024-11-17 18:28:54 -0300

    Create NavBar.js

---
+++ src/Frontend/Segunda Entrega/NavBar.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <button>Convênios Atendidos</button>
+      <button>Agendamento</button>
+      <button>Resultados</button>
+      <button>Farmácia</button>
+      <button>Educação</button>
+    </nav>
+  );
+}
+
+export default Navbar;
