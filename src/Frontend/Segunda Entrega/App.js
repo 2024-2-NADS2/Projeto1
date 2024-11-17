d5a7cbe21fc92671c9e94388cbeb1501d9a05e0e
Luiza Domingues Chaveiro Correia <163072410+luiza1205@users.noreply.github.com>
2024-11-17 19:34:25 -0300

    Update App.js

--- src/Frontend/Segunda Entrega/App.js
+++ src/Frontend/Segunda Entrega/App.js
@@ -1,11 +1,25 @@
-import React from "react";
-import LoginPage from "./components/LoginPage";
-import "./App.css";
-function App() {
-return (
-<div className="App">
-<LoginPage />
-</div>
-);
-}
-export default App;
+// app.js
+const express = require("express");
+const bodyParser = require("body-parser");
+const sequelize = require("./config/database");
+const Paciente = require("./models/Paciente");
+const Medico = require("./models/Medico");
+const Consulta = require("./models/Consulta");
+
+const pacienteRoutes = require("./routes/pacienteRoutes");
+const medicoRoutes = require("./routes/medicoRoutes");
+const consultaRoutes = require("./routes/consultaRoutes");
+
+const app = express();
+app.use(bodyParser.json());
+
+app.use("/pacientes", pacienteRoutes);
+app.use("/medicos", medicoRoutes);
+app.use("/consultas", consultaRoutes);
+
+sequelize.sync().then(() => {
+  console.log("Banco de dados sincronizado");
+  app.listen(3000, () => {
+    console.log("Servidor rodando na porta 3000");
+  });
+});
